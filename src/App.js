c73b5b68bc78fa93bb3cd8d9ce1c37e0f9960963
Senuji Pathirage <cb010982@students.apiit.lk>
2023-05-14 22:13:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,56 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login  />
     </div>
   );
 }
 
-export default App;
+
+function Login(props) {
+  //use props to get the input from dropdown
+  const choice = props.value; //value will be passed from dropd
+  let str = "";
+  switch (choice) {
+    case "1":
+      str = <h2>Donor Login </h2>;
+      break;
+    case "2":
+      str = <h2>BloodBank Login</h2>;
+      break;
+    case "3":
+      str = <h2>Hospital Login </h2>;
+      break;
+    case "4":
+      str = <h2>Administrator Login</h2>;
+      break;
+    default:
+      return (
+        <div>
+          <p>Something went Wrong. Please go back or refresh the page.</p>
+        </div>
+      );
+  }
+
+  return (
+    <div className={styles.form}>
+      <div>{str}</div>
+      <div>
+        <label for="username">Username :</label>
+        <input type="text" id="username" />
+      </div>
+      <div>
+        <label for="pwd">Password :</label>
+        <input type="password" id="pwd" />
+      </div>
+      <div>
+        <button id="btnLogin">Login</button>
+      </div>
+    </div>
+  );
+}
+
+Login.defaultProps = {
+  value: "0",
+};
+export default Login;
